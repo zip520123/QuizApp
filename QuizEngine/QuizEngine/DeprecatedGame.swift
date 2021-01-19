f820ad84131fa0a41b068e560b8df60b989f0219
zip520123 <zip520123@yahoo.com.tw>
2021-01-19 20:54:24 +0000

    Move Router protocol to deprecated components file.

--- QuizEngine/QuizEngine/DeprecatedGame.swift
+++ QuizEngine/QuizEngine/DeprecatedGame.swift
@@ -7,6 +7,16 @@
 //
 
 import Foundation
+
+@available(*, deprecated)
+public protocol Router {
+    associatedtype Question: Hashable
+    associatedtype Answer
+    
+    func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
+    func routeTo(result: Result<Question, Answer>)
+}
+
 @available (*, deprecated)
 public class Game<Question, Answer, R:Router> {
     let flow: Any
