1ed2f7cd2e8c12ca876162b4a4b2e7b875d1d6b9
zip520123 <zip520123@yahoo.com.tw>
2021-01-19 20:52:36 +0000

    Move delegate to its own file.

--- QuizEngine/QuizEngine/Router.swift
+++ QuizEngine/QuizEngine/Router.swift
@@ -3,14 +3,7 @@
 //
 
 import Foundation
-public protocol QuizDelegate {
-    associatedtype Question: Hashable
-    associatedtype Answer
-    
-    func handle(question: Question, answerCallback: @escaping (Answer) -> Void)
-    func handle(result: Result<Question, Answer>)
 
-}
 
 @available(*, deprecated)
 public protocol Router {
