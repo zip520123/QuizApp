f361edccf8ed60d6fb32a15ee92899ecd8df80f3
zip520123 <zip520123@yahoo.com.tw>
2021-01-28 18:10:29 +0000

    Clarify the test name.

--- QuizEngine/QuizEngineTests/QuizTest.swift
+++ QuizEngine/QuizEngineTests/QuizTest.swift
@@ -13,7 +13,7 @@ import QuizEngine
 class QuizTest: XCTestCase {
     
     private var quiz: Quiz!
-    func test_startQuiz_answerZeroOutOfTwoCorrectly_scoresZero() {
+    func test_startQuiz_answersAllQuestions_completesWithAnswers() {
         let delegate = DelegateSpy()
         quiz = Quiz.start(questions: ["Q1","Q2"], delegate: delegate)
         delegate.answerCompletion("A1")
