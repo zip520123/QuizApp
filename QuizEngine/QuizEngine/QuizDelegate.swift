9ba1fccbba5ad6f7c465f3f75b666069bc3ac4b7
zip520123 <zip520123@yahoo.com.tw>
2021-02-04 13:12:05 +0000

    Add QuizDataSource

--- QuizEngine/QuizEngine/QuizDelegate.swift
+++ QuizEngine/QuizEngine/QuizDelegate.swift
@@ -3,13 +3,17 @@
 //
 
 import Foundation
-public protocol QuizDelegate {
+public protocol QuizDataSource {
     associatedtype Question
     associatedtype Answer
     
     func answer(for question: Question, completion: @escaping (Answer) -> Void)
-    
+}
+
+public protocol QuizDelegate {
+    associatedtype Question
+    associatedtype Answer
+
     func didCompleteQuiz(withAnswers: [(question: Question, answer: Answer)])
-    
-    
+    func answer(for question: Question, completion: @escaping (Answer) -> Void)
 }
