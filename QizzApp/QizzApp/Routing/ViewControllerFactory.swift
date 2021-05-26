e15b4a22f8c658cf372289b812ca3f31d7632564
zip520123 <zip520123@yahoo.com.tw>
2021-05-26 11:50:46 +0100

    fix tuple names mismatch

--- QizzApp/QizzApp/Routing/ViewControllerFactory.swift
+++ QizzApp/QizzApp/Routing/ViewControllerFactory.swift
@@ -6,10 +6,10 @@ import UIKit
 import QuizEngine
 
 protocol ViewControllerFactory {
-    typealias Answers = [(question: Question<String>, answers: [String])]
+    typealias Answers = [(question: Question<String>, answer: [String])]
     func questionViewController(for question: Question<String>, answerCallback:@escaping ([String])->Void) -> UIViewController
     func resultViewController(for result: Result<Question<String>, [String]>) -> UIViewController
-    func resultViewController(for userAnswers: [(question: Question<String>, answers: [String])]) -> UIViewController
+    func resultViewController(for userAnswers: [(question: Question<String>, answer: [String])]) -> UIViewController
 }
 
 
