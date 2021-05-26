08bd312d7a2ed9fccdb579e68349070ca40822b0
zip520123 <zip520123@yahoo.com.tw>
2021-05-26 12:06:55 +0100

    Removed deprecated methoed

--- QizzApp/QizzApp/Routing/ViewControllerFactory.swift
+++ QizzApp/QizzApp/Routing/ViewControllerFactory.swift
@@ -8,7 +8,6 @@ import QuizEngine
 protocol ViewControllerFactory {
     typealias Answers = [(question: Question<String>, answer: [String])]
     func questionViewController(for question: Question<String>, answerCallback:@escaping ([String])->Void) -> UIViewController
-    func resultViewController(for result: Result<Question<String>, [String]>) -> UIViewController
     func resultViewController(for userAnswers: [(question: Question<String>, answer: [String])]) -> UIViewController
 }
 
