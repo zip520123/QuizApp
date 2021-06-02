b160f9673dcc35f699173ee699bb0ec1714c38b2
zip520123 <zip520123@yahoo.com.tw>
2021-06-02 17:03:35 +0100

    Toggle selection state.

--- QizzApp/QizzApp/AppDelegate.swift
+++ QizzApp/QizzApp/AppDelegate.swift
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let options =  [question1: options1, question2: options2]
         let correctAnswers = [(question1,[option3]),(question2, [option4, option6]) ]
         let navigationController = UINavigationController()
-        let factory = iOSUIKitViewControllerFactory(options:options, correctAnswers: correctAnswers)
+        let factory = iOSSwiftUIViewControllerFactory(options:options, correctAnswers: correctAnswers)
         let router = NavigationControllerRouter(navigationController, factory: factory)
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
---
+++ QizzApp/QizzAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -0,0 +1,34 @@
+//
+//  MultipleSelectionStoreTests.swift
+//  QizzAppTests
+//
+//  Created by zip520123 on 02/06/2021.
+//  Copyright © 2021 zip520123. All rights reserved.
+//
+
+import XCTest
+struct MultipleSelectionStore {
+    var options: [MultipleSelectionOption]
+    internal init(options: [String]) {
+        self.options = options.map {MultipleSelectionOption(text: $0)}
+    }
+}
+
+struct MultipleSelectionOption {
+    let text: String
+    var isSelected = false
+    mutating func select() {
+        isSelected.toggle()
+    }
+}
+
+class MultipleSelectionStoreTests: XCTestCase {
+    func test() {
+        var sut = MultipleSelectionStore(options: ["o0", "o1"])
+        XCTAssertFalse(sut.options[0].isSelected)
+        sut.options[0].select()
+        XCTAssertTrue(sut.options[0].isSelected)
+        sut.options[0].select()
+        XCTAssertFalse(sut.options[0].isSelected)
+    }
+}
