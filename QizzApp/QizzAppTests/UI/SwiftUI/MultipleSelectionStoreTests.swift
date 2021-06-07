4a9f0494825e8e4c48ed75cbcc068a8dc474851c
zip520123 <zip520123@yahoo.com.tw>
2021-06-07 12:46:16 +0100

    Can submit when at least one option is selected

--- QizzApp/QizzAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
+++ QizzApp/QizzAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -8,6 +8,10 @@
 
 import XCTest
 struct MultipleSelectionStore {
+    var canSubmit : Bool {
+        options.filter{ $0.isSelected }.endIndex > 0
+    }
+    
     var options: [MultipleSelectionOption]
     internal init(options: [String]) {
         self.options = options.map {MultipleSelectionOption(text: $0)}
@@ -30,5 +34,16 @@ class MultipleSelectionStoreTests: XCTestCase {
         XCTAssertTrue(sut.options[0].isSelected)
         sut.options[0].select()
         XCTAssertFalse(sut.options[0].isSelected)
+    }
+    func test_canSubmit_whenAtLeastOneOptionIsSelected() {
+        var sut = MultipleSelectionStore(options: ["o0", "o1"])
+        XCTAssertFalse(sut.canSubmit)
+        sut.options[0].select()
+        XCTAssertTrue(sut.canSubmit)
+        sut.options[0].select()
+        XCTAssertFalse(sut.canSubmit)
+        sut.options[1].select()
+        XCTAssertTrue(sut.canSubmit)
+        
     }
 }
