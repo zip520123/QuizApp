6ef0aa28e2bc2ef702b26bc8761b363d494a69aa
zip520123 <zip520123@yahoo.com.tw>
2021-06-08 11:22:23 +0100

    Add multipleAnswerQuestion

---
+++ QizzApp/QizzApp/UI/SwiftUI/MultipleAnswerQuestion.swift
@@ -0,0 +1,71 @@
+//
+//  MultipleAnswerQuestion.swift
+//  QizzApp
+//
+//  Created by zip520123 on 07/06/2021.
+//  Copyright © 2021 zip520123. All rights reserved.
+//
+
+import SwiftUI
+
+struct MultipleAnswerQuestion: View {
+    let title: String
+    let question: String
+    
+    @State var store: MultipleSelectionStore
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0.0) {
+            QuestionHeader(title: title, question: question)
+            
+            ForEach(store.options.indices) { (i) in
+                MultipleTextSelectionCell(option: $store.options[i])
+            }
+            Spacer()
+            Button(action: store.submit, label: {
+                HStack{
+                    Spacer()
+                    Text("Submit")
+                        .padding()
+                        .foregroundColor(.white)
+                    Spacer()
+                }
+                .background(Color.blue)
+                .cornerRadius(25)
+                
+            })
+            .buttonStyle(PlainButtonStyle())
+            .padding()
+            .disabled(!store.canSubmit)
+        }
+    }
+}
+
+struct MultipleAnswerQuestion_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            MultipleAnswerQuestionTestView()
+            
+            MultipleAnswerQuestionTestView()
+                .preferredColorScheme(.dark).environment(\.sizeCategory, .extraExtraLarge)
+        }
+        
+    }
+    struct MultipleAnswerQuestionTestView: View {
+        @State var selection = ["none"]
+        var body: some View {
+            VStack {
+                MultipleAnswerQuestion(
+                    title: "1 of 2",
+                    question: "What'sM Mike's name",
+                    store: .init(
+                        options: ["Portuguess", "American", "Geek"
+                        ], handler: { selection = $0
+                        } )
+                )
+                Text("Last selection: " + selection.joined(separator: ", "))
+            }
+            
+        }
+    }
+}
